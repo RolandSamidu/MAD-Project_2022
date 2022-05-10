cb66690bc52ccc7d0d1fef90f995656696adc327
RolandSamidu <samidu.appuhamy@gmail.com>
2022-05-11 04:35:06 +0530

    Updated

--- adminapp/src/main/java/com/rsd_tecnology/adminapp/CHDRActivity.java
+++ adminapp/src/main/java/com/rsd_tecnology/adminapp/CHDRActivity.java
@@ -2,13 +2,28 @@ package com.rsd_tecnology.adminapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class CHDRActivity extends AppCompatActivity {
+    private Button immunization;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chdractivity);
+
+        immunization = findViewById(R.id.immunization);
+
+        immunization.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(CHDRActivity.this, ImmunizationActivity.class));
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/rsd_tecnology/babycare/CustomerHomeActivity.java
+++ app/src/main/java/com/rsd_tecnology/babycare/CustomerHomeActivity.java
@@ -4,11 +4,16 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class CustomerHomeActivity extends AppCompatActivity {
+    private Button report;
+    private Button blog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,24 @@ public class CustomerHomeActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Baby Care");
         actionBar.setSubtitle("Home");
+
+        report = findViewById(R.id.report);
+        blog = findViewById(R.id.Blog);
+
+        report.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(CustomerHomeActivity.this , CHDRActivity.class));
+                finish();
+            }
+        });
+
+        blog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(CustomerHomeActivity.this , BlogActivity.class));
+            }
+        });
     }
 
     @Override
@@ -26,11 +49,4 @@ public class CustomerHomeActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.logout:
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }

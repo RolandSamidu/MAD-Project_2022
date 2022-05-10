e3d789ef3f1bd1a1e10a4b1a4744252acb08b230
RolandSamidu <samidu.appuhamy@gmail.com>
2022-05-11 01:16:37 +0530

    Add to Immunization Part

---
+++ adminapp/src/main/java/com/rsd_tecnology/adminapp/ImmunizationActivity.java
@@ -0,0 +1,14 @@
+package com.rsd_tecnology.adminapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImmunizationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_immunization);
+    }
+}
